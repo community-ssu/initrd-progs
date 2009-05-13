24fde0111a77aca2b477670633c2fedf5fd411bc
Marat Radchenko <marat@slonopotamus.org>
2009-05-13 09:07:38 +0400

    use size_t/ssize_t for I/O

--- open-wlan-cal.c
+++ open-wlan-cal.c
@@ -30,10 +30,10 @@
 
 const char DSME_DEFAULT_SOCKET_PATH[] = "/tmp/dsmesock";
 
-int get_from_dsme(const void *request, const int bytes_send, 
-		void *buff, const int bytes_read) {
+ssize_t get_from_dsme(const void *request, const size_t bytes_send, 
+		void *buff, const size_t bytes_read) {
 	int sock;
-	int ret;
+	ssize_t ret;
 	struct sockaddr_un sockaddr;
 
 	/* create socket */
@@ -59,7 +59,7 @@ int get_from_dsme(const void *request, const int bytes_send,
 		perror("Could not send dsme request");
 		close(sock);
 		return -1;
-	} else if (ret != bytes_send) {
+	} else if ((size_t)ret != bytes_send) {
 		fprintf(stderr, "Could write only %d bytes out of %d", ret, bytes_send);
 		perror(NULL);
 		close(sock);
@@ -70,7 +70,7 @@ int get_from_dsme(const void *request, const int bytes_send,
 	ret = read(sock, buff, bytes_read);
 	if (ret == -1) {
 		perror("Could not read dsme response");
-	} else if (ret != bytes_read) {
+	} else if ((size_t)ret != bytes_read) {
 		fprintf(stderr, "Could read only %d bytes out of %d", ret, bytes_read);
 		perror(NULL);
 	}
@@ -80,9 +80,9 @@ int get_from_dsme(const void *request, const int bytes_send,
 	return ret;
 }
 
-int write_to(const char filename[], const void *value, const int len) {
+ssize_t write_to(const char filename[], const void *value, const size_t len) {
 	int f;
-	int ret;
+	ssize_t ret;
 	if ((f = open(filename, O_WRONLY)) == -1) {
 		fprintf(stderr, "Could not open file %s", filename);
 		perror(NULL);
@@ -92,7 +92,7 @@ int write_to(const char filename[], const void *value, const int len) {
 	if (ret == -1) {
 		fprintf(stderr, "Could not write data to %s", filename);
 		perror(NULL);
-	} else if (ret != len) {
+	} else if ((size_t)ret != len) {
 		fprintf(stderr, "Could write only %d bytes out of %d", ret, len);
 		perror(NULL);
 	}
@@ -100,45 +100,48 @@ int write_to(const char filename[], const void *value, const int len) {
 	return ret;
 }
 
-int set_mac(const void *value, const int len) {
+ssize_t set_mac(const void *value, const size_t len) {
 	return write_to("/sys/devices/platform/wlan-omap/cal_mac_address", value, len);
 }
 
-int set_default_country(const void *value, const int len) {
+ssize_t set_default_country(const void *value, const size_t len) {
 	return write_to("sys/devices/platform/wlan-omap/default_country", value, len);
 }
 
-int set_tx_limits(const void *value, const int len) {
+ssize_t set_tx_limits(const void *value, const size_t len) {
 	return write_to("/sys/devices/platform/wlan-omap/cal_output_limits",
 		value, len);
 }
 
-int set_iq_values(const void *value, const int len) {
+ssize_t set_iq_values(const void *value, const size_t len) {
 	return write_to("/sys/devices/platform/wlan-omap/cal_iq", value, len);
 }
 
-int set_tx_tuned_data(const void *value, const int len) {
+ssize_t set_tx_tuned_data(const void *value, const size_t len) {
 	return write_to("/sys/devices/platform/wlan-omap/cal_pa_curve_data",
 		value, len);
 }
 
-int set_rx_tuned_data(const void *value, const int len) {
+ssize_t set_rx_tuned_data(const void *value, const size_t len) {
 	return write_to("/sys/devices/platform/wlan-omap/cal_rssi", value, len);
 }
 
 void load_from_dsme() {
+	/* TODO: use struct for request (and, possibly, for response header) */
 	const char mac_request[]
 		= " \0\0\0\0\22\0\0wlan-mac\0\0\0\0\0\0\0\0\0\0\0\0\10 \1";
 	char mac_address_data[60];
-	char mac_address[6];
-	int len;
-	int ret;
+	size_t len;
+	ssize_t ret;
 	len = sizeof(mac_address_data);
 	printf("Pushing MAC address...");
 	fflush(stdout);
 	ret = get_from_dsme(mac_request, sizeof(mac_request), mac_address_data, len);
-	if (ret == len) {
-		unsigned int i;
+	if (ret == -1) {
+		return;
+	} else if ((size_t)ret == len) {
+		size_t i;
+		char mac_address[6];
 		for (i = 0; i < sizeof(mac_address); ++i) {
 			mac_address[i] = mac_address_data[32 + 4 * i];
 		}
