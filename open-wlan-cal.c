community-ssu/initrd-progs

commit 369255c4035c0e6d0579bf82ea98c1462a17e69d

more verbose usage docs, minor refactorings

open-wlan-cal.c
@@ -31,8 +31,11 @@
 #include <sys/ioctl.h>
 #include <mtd/mtd-user.h>
 
-ssize_t get_from_dsme(const char *socket_path, const void *request,
-		const size_t bytes_send, void *buf, const size_t bytes_read, const size_t bytes_skip) {
+const char *default_dsme_path = "/tmp/dsmesock";
+const char *default_direct_access_path = "/dev/mtd1";
+
+ssize_t get_from_dsme(const char *path, const void *request, const size_t bytes_send,
+		void *buf, const size_t bytes_read, const size_t bytes_skip) {
 	const size_t total_read = bytes_skip + bytes_read;
 	char data[total_read];
 	int sock;
@@ -47,9 +50,10 @@ ssize_t get_from_dsme(const char *socket_path, const void *request,
 
 	/* connect */
 	sockaddr.sun_family = AF_FILE;
-	/* TODO: check length (only 108 chars allowed) */
-	strcpy(sockaddr.sun_path, socket_path);
-	ret = strlen(sockaddr.sun_path) + sizeof(sockaddr.sun_family);
+	assert(strlen(path) <= sizeof(sockaddr.sun_path));
+	/* TODO: test if it works correctly with 108-char path. */
+	strcpy(sockaddr.sun_path, path);
+	ret = sizeof(sockaddr.sun_path) + sizeof(sockaddr.sun_family);
 	if (connect(sock, (struct sockaddr *)&sockaddr, ret) == -1) {
 		perror("Could not connect to socket");
 		close(sock);
@@ -62,44 +66,36 @@ ssize_t get_from_dsme(const char *socket_path, const void *request,
 		perror("Could not send dsme request");
 		close(sock);
 		return -1;
-	} else if ((size_t)ret != bytes_send) {
-		fprintf(stderr, "Could write only %zd bytes out of %zd", ret, bytes_send);
-		perror(NULL);
-		close(sock);
-		return -1;
 	}
+	assert((size_t)ret == bytes_send);
 
 	/* read response */
-	ret = read(sock, data, total_read);
+	ret = read(sock, &data[ret], total_read);
 	if (ret == -1) {
 		perror("Could not read dsme response");
-	} else if ((size_t)ret != total_read) {
-		fprintf(stderr, "Could read only %zd bytes out of %zd", ret, total_read);
-		perror(NULL);
 		close(sock);
 		return -1;
 	}
+	assert((size_t)ret == total_read);
 	memcpy(buf, &data[bytes_skip], bytes_read);
-
-	/* cleanup */
 	close(sock);
 	return ret - bytes_skip;
 }
 
-ssize_t write_to(const char *filename, const void *value, const size_t len) {
+ssize_t write_to(const char *path, const void *value, const size_t len) {
 	int f;
 	ssize_t ret;
-	if ((f = open(filename, O_WRONLY)) == -1) {
-		fprintf(stderr, "Could not open file %s", filename);
+	if ((f = open(path, O_WRONLY)) == -1) {
+		fprintf(stderr, "Could not open file %s: ", path);
 		perror(NULL);
 		return -1;
 	}
 	ret = write(f, value, len);
 	if (ret == -1) {
-		fprintf(stderr, "Could not write data to %s", filename);
+		fprintf(stderr, "Could not write data to %s: ", path);
 		perror(NULL);
 	} else if ((size_t)ret != len) {
-		fprintf(stderr, "Could write only %zd bytes out of %zd", ret, len);
+		fprintf(stderr, "Could write only %zd bytes out of %zd: ", ret, len);
 		perror(NULL);
 	}
 	close(f);
@@ -133,7 +129,9 @@ void set_default_country() {
 ssize_t get_mac_direct(const char *path, char *buf, const size_t len) {
 	int f;
 	ssize_t ret;
+	struct mtd_info_user info;
 	if ((f = open(path, O_RDONLY)) == -1) {
+		fprintf(stderr, "Could not open file %s: ", path);
 		perror(NULL);
 		return -1;
 	}
@@ -159,8 +157,15 @@ ssize_t get_mac_direct(const char *path, char *buf, const size_t len) {
 		ECCGETSTATS				0x80104d12
 		MTDFILEMODE				0x4d13
 	*/
-	ioctl(f, MTDFILEMODE);
-	puts("[Not implemented yet]");
+	/* Warning! If this is uncommented, segfault happens without -O optimisations,
+		and hang on futex(0x4015a020, FUTEX_WAIT, 2, NULL with -Os/-O2.
+		I don't understand where futex comes from at all yet.
+	*/
+	if (ioctl(f, MEMGETINFO, info) == -1) {
+		perror(NULL);
+		close(f);
+		return -1;
+	}
 	ret = -1;
 	close(f);
 	return ret;
@@ -316,15 +321,17 @@ void set_rx_values() {
 }
 
 int usage(const char *progname) {
-	fprintf(stderr, "Usage: %s [-d] [PATH]\n", progname);
-	puts("\t-d\tif specified, data is read directly instead of via dsme server.");
-	puts("\tPATH\tspecifies where data will be read from.");
+	fprintf(stderr, "Usage: %s [-d] [PATH]\n"
+		"  -d\tIf specified, data is read directly from mtd partition"
+		" instead of dsme server socket.\n"
+		"  PATH\tSpecifies where path to mtd partition if -d option is used"
+		" or to dsme server socket path otherwise.\n"
+		"\tIf no value is specified, %s is used in direct access mode and %s in dsme.\n",
+		progname, default_direct_access_path, default_dsme_path);
 	return EXIT_FAILURE;
 }
 
 int main(const int argc, char *argv[]) {
-	const char *default_dsme_path = "/tmp/dsmesock";
-	const char *default_direct_access_path = "/dev/mtd1";
 	const char *progname = argv[0];
 	const char *path;
 	int opt;

commit fe3a9d65719400884c23fbc8d1bfc111929a2c3d

almost working direct mac address reading, fixed offset bug in get_from_dsme that was introduced in commit 369255c4035c0e6d0579bf82ea98c1462a17e69d

open-wlan-cal.c
@@ -34,16 +34,72 @@
 const char *default_dsme_path = "/tmp/dsmesock";
 const char *default_direct_access_path = "/dev/mtd1";
 
-ssize_t get_from_dsme(const char *path, const void *request, const size_t bytes_send,
-		void *buf, const size_t bytes_read, const size_t bytes_skip) {
+size_t skip_and_read(const int fd, void *buf, const size_t bytes_read,
+		const size_t bytes_skip) {
 	const size_t total_read = bytes_skip + bytes_read;
 	char data[total_read];
-	int sock;
+	ssize_t ret;
+	ret = read(fd, &data, total_read);
+	assert((size_t)ret == total_read);
+	memcpy(buf, &data[bytes_skip], bytes_read);
+	return ret - bytes_skip;
+}
+
+size_t get_from_mtd(const char *path, void *buf, const size_t bytes_read,
+		const size_t bytes_skip) {
+	const int mode = MTD_OTP_USER;
+	ssize_t ret;
+	int fd;
+	if ((fd = open(path, O_RDONLY)) == -1) {
+		fprintf(stderr, "Could not open file %s: ", path);
+		perror(NULL);
+		return -1;
+	}
+	/* Sadly, strace doesn't know mtd ioctl codes and misinterprets them as mtrr codes.
+		Here's mapping table (first column is real code, second - what strace thinks it is):
+		MEMGETINFO				MTRRIOC_SET_ENTRY
+		MEMERASE					MTRRIOC_DEL_ENTRY
+		MEMWRITEOOB				MTRRIOC_GET_ENTRY
+		MEMREADOOB				MTRRIOC_KILL_ENTRY
+		MEMLOCK						MTRRIOC_ADD_PAGE_ENTRY
+		MEMUNLOCK					MTRRIOC_SET_PAGE_ENTRY
+		MEMGETREGIONCOUNT MTRRIOC_DEL_PAGE_ENTRY
+		MEMGETREGIONINFO	MTRRIOC_GET_PAGE_ENTRY
+		MEMSETOOBSEL			MTRRIOC_KILL_PAGE_ENTRY
+		MEMGETOOBSEL			0x80c84d0a
+		MEMGETBADBLOCK		0x40084d0b
+		MEMSETBADBLOCK		0x40084d0c
+		OTPSELECT					0x80044d0d
+		OTPGETREGIONCOUNT 0x40044d0e
+		OTPGETREGIONINFO	0x400c4d0f
+		OTPLOCK						0x800c4d10
+		ECCGETLAYOUT			0x81484d11
+		ECCGETSTATS				0x80104d12
+		MTDFILEMODE				0x4d13
+	*/
+	if (ioctl(fd, OTPSELECT, &mode) == -1) {
+		perror(NULL);
+		close(fd);
+		return -1;
+	}
+	if (lseek(fd, 0, SEEK_SET) == -1) {
+		perror(NULL);
+		close(fd);
+		return -1;
+	}
+	ret = skip_and_read(fd, buf, bytes_read, bytes_skip);
+	close(fd);
+	return ret;
+}
+
+ssize_t get_from_dsme(const char *path, const void *request, const size_t bytes_send,
+		void *buf, const size_t bytes_read, const size_t bytes_skip) {
+	int fd;
 	ssize_t ret;
 	struct sockaddr_un sockaddr;
 
 	/* create socket */
-	if ((sock = socket(AF_LOCAL, SOCK_STREAM, 0)) == -1) {
+	if ((fd = socket(AF_LOCAL, SOCK_STREAM, 0)) == -1) {
 		perror("Could not create socket");
 		return -1;
 	}
@@ -54,32 +110,24 @@ ssize_t get_from_dsme(const char *path, const void *request, const size_t bytes_
 	/* TODO: test if it works correctly with 108-char path. */
 	strcpy(sockaddr.sun_path, path);
 	ret = sizeof(sockaddr.sun_path) + sizeof(sockaddr.sun_family);
-	if (connect(sock, (struct sockaddr *)&sockaddr, ret) == -1) {
+	if (connect(fd, (struct sockaddr *)&sockaddr, ret) == -1) {
 		perror("Could not connect to socket");
-		close(sock);
+		close(fd);
 		return -1;
 	}
 
 	/* send request */
-	ret = write(sock, request, bytes_send);
+	ret = write(fd, request, bytes_send);
 	if (ret == -1) {
 		perror("Could not send dsme request");
-		close(sock);
+		close(fd);
 		return -1;
 	}
 	assert((size_t)ret == bytes_send);
 
-	/* read response */
-	ret = read(sock, &data[ret], total_read);
-	if (ret == -1) {
-		perror("Could not read dsme response");
-		close(sock);
-		return -1;
-	}
-	assert((size_t)ret == total_read);
-	memcpy(buf, &data[bytes_skip], bytes_read);
-	close(sock);
-	return ret - bytes_skip;
+	ret = skip_and_read(fd, buf, bytes_read, bytes_skip);
+	close(fd);
+	return ret;
 }
 
 ssize_t write_to(const char *path, const void *value, const size_t len) {
@@ -126,53 +174,16 @@ void set_default_country() {
 
 /* MAC */
 
-ssize_t get_mac_direct(const char *path, char *buf, const size_t len) {
-	int f;
-	ssize_t ret;
-	struct mtd_info_user info;
-	if ((f = open(path, O_RDONLY)) == -1) {
-		fprintf(stderr, "Could not open file %s: ", path);
-		perror(NULL);
-		return -1;
-	}
-	/* Sadly, strace doesn't know mtd ioctl codes and misinterprets them as mtrr codes.
-		Here's mapping table (first column is real code, second - what strace thinks it is):
-		MEMGETINFO				MTRRIOC_SET_ENTRY
-		MEMERASE					MTRRIOC_DEL_ENTRY
-		MEMWRITEOOB				MTRRIOC_GET_ENTRY
-		MEMREADOOB				MTRRIOC_KILL_ENTRY
-		MEMLOCK						MTRRIOC_ADD_PAGE_ENTRY
-		MEMUNLOCK					MTRRIOC_SET_PAGE_ENTRY
-		MEMGETREGIONCOUNT MTRRIOC_DEL_PAGE_ENTRY
-		MEMGETREGIONINFO	MTRRIOC_GET_PAGE_ENTRY
-		MEMSETOOBSEL			MTRRIOC_KILL_PAGE_ENTRY
-		MEMGETOOBSEL			0x80c84d0a
-		MEMGETBADBLOCK		0x40084d0b
-		MEMSETBADBLOCK		0x40084d0c
-		OTPSELECT					0x80044d0d
-		OTPGETREGIONCOUNT 0x40044d0e
-		OTPGETREGIONINFO	0x400c4d0f
-		OTPLOCK						0x800c4d10
-		ECCGETLAYOUT			0x81484d11
-		ECCGETSTATS				0x80104d12
-		MTDFILEMODE				0x4d13
-	*/
-	if (ioctl(f, MEMGETINFO, &info) == -1) {
-		perror(NULL);
-		close(f);
-		return -1;
-	}
-	ret = -1;
-	close(f);
-	return ret;
+ssize_t get_mac_direct(const char *path, void *buf, const size_t len) {
+	return get_from_mtd(path, buf, len, 12);
 }
 
-ssize_t get_mac_from_dsme(const char *path, char *buf, const size_t len) {
+ssize_t get_mac_from_dsme(const char *path, void *buf, const size_t len) {
 	const char *req = " \0\0\0\0\22\0\0wlan-mac\0\0\0\0\0\0\0\0\0\0\0\0\10 \1\0";
 	return get_from_dsme(path, req, 32, buf, len, 36);
 }
 
-void set_mac(const char *path, ssize_t (*get)(const char *, char *, const size_t len)) {
+void set_mac(const char *path, ssize_t (*get)(const char *, void *, const size_t len)) {
 	const size_t mac_len = 6;
 	const char *file = "/sys/devices/platform/wlan-omap/cal_mac_address";
 	char mac_address[mac_len];
@@ -183,6 +194,7 @@ void set_mac(const char *path, ssize_t (*get)(const char *, char *, const size_t
 		size_t i;
 		for (i = 0; i < mac_len; ++i) {
 			const size_t idx = 4 * i;
+			assert(idx < sizeof(input));
 			mac_address[i] = input[idx];
 		}
 		print_end(write_to(file, mac_address, mac_len));
@@ -191,19 +203,19 @@ void set_mac(const char *path, ssize_t (*get)(const char *, char *, const size_t
 
 /* IQ values */
 
-ssize_t get_iq_values_direct(const char *path, char *buf, const size_t len) {
+ssize_t get_iq_values_direct(const char *path, void *buf, const size_t len) {
 	/* TODO: implement this */
 	puts("[Not implemented yet]");
 	return -1;
 }
 
-ssize_t get_iq_values_from_dsme(const char *path, char *buf, const size_t len) {
+ssize_t get_iq_values_from_dsme(const char *path, void *buf, const size_t len) {
 	const char *req = " \0\0\0\0\22\0\0wlan-iq-align\0\0\0\0\0\0\0\10 \1\0";
 	return get_from_dsme(path, req, 32, buf, len, 28);
 }
 
 void set_iq_values(const char *path,
-		ssize_t (*get)(const char *, char *, const size_t len)) {
+		ssize_t (*get)(const char *, void *, const size_t len)) {
 	/* 14 (items + 1 empty) * 8 (read_item_len) */
 	char input[112];
 	print_start("Pushing IQ tuned values...");
@@ -232,19 +244,19 @@ void set_iq_values(const char *path,
 
 /* TX curve data */
 
-ssize_t get_tx_curve_direct(const char *path, char *buf, const size_t len) {
+ssize_t get_tx_curve_direct(const char *path, void *buf, const size_t len) {
 	/* TODO: implement this */
 	puts("[Not implemented yet]");
 	return -1;
 }
 
-ssize_t get_tx_curve_from_dsme(const char *path, char *buf, const size_t len) {
+ssize_t get_tx_curve_from_dsme(const char *path, void *buf, const size_t len) {
 	const char *req = " \0\0\0\0\22\0\0wlan-tx-gen2\0\0\0\0\0\0\0\0\10 \1\0";
 	return get_from_dsme(path, req, 32, buf, len, 46);
 }
 
 void set_tx_curve(const char *path,
-		ssize_t (*get)(const char *, char *, const size_t len)) {
+		ssize_t (*get)(const char *, void *, const size_t len)) {
 	/* 38 * 13 (items) */
 	char input[494];
 	print_start("Pushing TX tuned values...");