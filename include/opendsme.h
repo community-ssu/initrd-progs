8f713039a324c6d1f6fe9aa52fb243bdc39fee98
Marat Radchenko <marat@slonopotamus.org>
2009-07-23 19:53:36 +0400

    initial CAL api

---
+++ include/opendsme.h
@@ -0,0 +1,24 @@
+/*
+	Copyright 2009, Marat Radchenko
+
+	This file is part of opendsme.
+
+	opendsme is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	opendsme is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with opendsme.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define DEFAULT_DSME_PATH "/tmp/dsmesock"
+
+
+size_t get_from_mtd(const char *path, void *buf, const off_t seek_to,
+	const size_t bytes_read, const size_t bytes_skip, const int mode);
